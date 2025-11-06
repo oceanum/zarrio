d7c7fe2c98a83af183796ef3be19dfd6b8945512
Tom Durrant <t.durrant@oceanum.science>
2025-11-06 15:52:35 +1100

    Bump version to 0.1.1

--- zarrio/__init__.py
+++ zarrio/__init__.py
@@ -2,7 +2,7 @@
 zarrio - A modern library for converting scientific data to Zarr format.
 """
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "Oceanum Developers"
 __email__ = "developers@oceanum.science"
 
